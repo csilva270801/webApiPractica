a7dc30c4779ab3a90c765f576744d0bd502f172b
csilva270801 <cescloborojo20@hotmail.com>
2023-02-25 23:13:11 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Models/equiposContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace webApiPractica.Models
+{
+    public class equiposContext: DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<equipos> equipos { get; set; }
+
+    }
+    
+}
