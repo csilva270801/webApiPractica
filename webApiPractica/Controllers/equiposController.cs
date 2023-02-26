810ad458d6fe5e57fe8fa68498d4465ebfabe3c4
csilva270801 <cescloborojo20@hotmail.com>
2023-02-26 00:11:02 -0600

    Agregar archivos de proyecto.

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -17,6 +17,8 @@ namespace webApiPractica.Controllers
             _equiposContexto = equiposContexto;
         }
 
+        //Retorna el listado de todos los equipos existentes
+
         [HttpGet]
         [Route("GetAll")]
         public IActionResult Get()
@@ -31,5 +33,98 @@ namespace webApiPractica.Controllers
 
             return Ok(listadoEquipo);
         }
+
+        //Retorna los registros de una tabla filtrados por su ID
+        
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            equipos? equipo =(from e in _equiposContexto.equipos
+                              where e.id_equipos = id
+                              select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        //Guarda nuevo registro
+
+        [HttpGet]
+        [Route("Find/{filtro}")]
+        public IActionResult FindyByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();  
+            }
+            return Ok(equipo);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+            try
+            {
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar) 
+        {
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                                     where e.id_equipos = id
+                                     select e).FirstOrDefault();
+            if(equipoActual == null)
+            {
+                return NotFound();
+            }
+
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+            
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public ActionResult EliminarEquipo(int id) 
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+                return NotFound();
+
+            _equiposContexto.equipos.Attach(equipo);
+            _equiposContexto.equipos.Remove(equipo);
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipo);
+        }
+        
+
     }
 }
